207e1db6642d9d6c4f1a2de7a756982231f269d6
Samaksh Mittal <samakshmittal826@gmail.com>
2023-10-24 22:25:51 +0530

    Create divideZero.java

---
+++ divideZero.java
@@ -0,0 +1,12 @@
+public class divideZero{
+	public static void main(String[] args){
+		try{
+			//code that generate exception
+			int divideByZero = 5/0;
+			System.out.println("Rest of code in try block");
+		}
+		catch(ArithmeticException e){
+			System.out.println("ArithmeticException => " + e.getMessage());
+		}
+	}
+}
